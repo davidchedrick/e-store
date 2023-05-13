bbebe9fd6665aa573837de791bbfbe3e37401281
David Chedrick <davidchedrick@gmail.com>
2023-05-12 23:30:10 -0500

    Added Product List

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,39 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header/Header";
+import ProductList from "./components/ProductList/ProductList";
+import ProductDetail from "./components/ProductDetail/ProductDetail";
+import Cart from "./components/Cart/Cart";
+import Checkout from "./components/Checkout/Checkout";
 
 function App() {
-	return <div className="App">Cat!!!!!!!!</div>;
+	return (
+		<Router>
+			<div className="App">
+				<Header />
+				<Switch>
+					<Route
+						path="/"
+						exact
+						component={ProductList}
+					/>
+					<Route
+						path="/product/:id"
+						component={ProductDetail}
+					/>
+					<Route
+						path="/cart"
+						component={Cart}
+					/>
+					<Route
+						path="/checkout"
+						component={Checkout}
+					/>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
